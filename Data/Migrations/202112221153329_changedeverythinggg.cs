dee47b0387fce9d60fffa83774771df5df4cc829
erikhawerman <84011896+erikhawerman@users.noreply.github.com>
2021-12-22 12:55:39 +0100

    fixeeddddd

---
+++ Data/Migrations/202112221153329_changedeverythinggg.cs
@@ -0,0 +1,18 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changedeverythinggg : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.CVs", "Creator", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.CVs", "Creator");
+        }
+    }
+}
